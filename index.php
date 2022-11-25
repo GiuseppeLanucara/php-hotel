63859d0c01b33bf5aff1156334b49004a6c5ba0a
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-11-25 17:13:57 +0100

    esercizio base completato

--- index.php
+++ index.php
@@ -48,10 +48,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-<table>
+    <table class="table table-dark table-hover">
         <tr>
             <th>Hotel Name</th>
             <th>Hotel Description</th>
@@ -67,7 +71,7 @@
             <tr>
                 <td><?php echo $subHotel['name']; ?></td>
                 <td><?php echo $subHotel['description']; ?></td>
-                <td><?php echo $subHotel['parking'] ?? "no parking"; ?></td>
+                <td><?php echo ($subHotel['parking'] ? "yes" : "no"); ?></td>
                 <td><?php echo $subHotel['vote']; ?></td>
                 <td><?php echo $subHotel['distance_to_center']; ?></td>
             </tr>
