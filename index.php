7a3626947718deb6eb55acc2eb54014d6a367638
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-11-27 19:10:59 +0100

    esercizio base finito con prova BONUS 1 ma non riuscito

--- index.php
+++ index.php
@@ -1,49 +1,50 @@
 <?php
-    $parkingFilter = $_GET["park"];
-    $hotels = [
+$parkingFilter = $_GET["park"];
+$hotels = [
 
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
 
-    ];
+];
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -54,9 +55,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Document</title>
 </head>
+
 <body>
     <form action="index.php" method="GET">
-        <label for="parking"><h3> Find only whit Parking </h3></label>
+        <label for="parking">
+            <h3> Find only whit Parking </h3>
+        </label>
         <input type="checkbox" name="park" id="parking">
     </form>
 
@@ -70,9 +74,8 @@
         </tr>
 
         <?php
-        foreach ($hotels as $subHotel)
-        {
-            ?>
+        foreach ($hotels as $subHotel) {
+        ?>
             <tr>
                 <td><?php echo $subHotel['name']; ?></td>
                 <td><?php echo $subHotel['description']; ?></td>
@@ -80,9 +83,10 @@
                 <td><?php echo $subHotel['vote']; ?></td>
                 <td><?php echo $subHotel['distance_to_center']; ?></td>
             </tr>
-            <?php
+        <?php
         }
         ?>
     </table>
 </body>
+
 </html>
